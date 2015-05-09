814f28898114633f06ef27a6f0210caf329ba1ad
The dev <ascottpedersen@gmail.com>
2015-05-09 17:58:22 -0400

    fdasfsdfds

--- db/migrate/20150509181841_add_fields_to_inquery.rb
+++
@@ -1,8 +0,0 @@
-class AddFieldsToInquery < ActiveRecord::Migration
-  def change
-    add_column :inquiries, :description, :text
-    add_column :inquiries, :name, :string
-    add_column :inquiries, :email, :string
-    add_column :inquiries, :phone, :string
-  end
-end
