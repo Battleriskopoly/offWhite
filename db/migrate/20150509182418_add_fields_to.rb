fe99aa025d6f2bcc8172f8d30ad118d538638b53
The dev <ascottpedersen@gmail.com>
2015-05-09 16:37:10 -0400

    fadsfds

--- db/migrate/20150509182418_add_fields_to.rb
+++ db/migrate/20150509182418_add_fields_to.rb
@@ -1,9 +1,5 @@
 class AddFieldsTo < ActiveRecord::Migration
   def change
-    remove_column :inquiries, :description, :text
-    remove_column :inquiries, :name, :string
-    remove_column :inquiries, :email, :string
-    remove_column :inquiries, :phone, :string
     add_column :static_pages, :description, :text
     add_column :static_pages, :name, :string
     add_column :static_pages, :email, :string
